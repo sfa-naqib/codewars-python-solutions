a49c643ba7ca753b122477b5d2f503f688df1d2b
naqib <naqibax619@gmail.com>
2026-02-18 20:10:38 +0600

    Create Calculating with Functions.py

---
+++ functional/Calculating with Functions.py
@@ -0,0 +1,15 @@
+def zero(func=None): return 0 if not func else func(0) #your code here
+def one(func=None): return 1 if not func else func(1) #your code here
+def two(func=None): return 2 if not func else func(2) #your code here
+def three(func=None): return 3 if not func else func(3) #your code here
+def four(func=None): return 4 if not func else func(4) #your code here
+def five(func=None): return 5 if not func else func(5) #your code here
+def six(func=None): return 6 if not func else func(6) #your code here
+def seven(func=None): return 7 if not func else func(7) #your code here
+def eight(func=None): return 8 if not func else func(8) #your code here
+def nine(func=None): return 9 if not func else func(9) #your code here
+
+def plus(y): return lambda x: x + y  #your code here
+def minus(y): return lambda x: x - y #your code here
+def times(y): return lambda x: x*y #your code here
+def divided_by(y): return lambda x: x // y #your code here
